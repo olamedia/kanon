74f46d474d1fb718d1669d44424d6dd516b5c77f
olamedia <olamedia@gmail.com>
2010-03-14 19:50:18 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@14 05b5fde2-2f63-11df-a735-6ff41f51f3aa

---
+++ kanon-framework.php
@@ -0,0 +1,10 @@
+<?php
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/registry.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/uri.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/controllerPrototype.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/controller.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/application.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/kanon.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/serviceController.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/applicationRegistry.php';
+require_once '/disk2/part2/__WWW/olamedia2010/www/kanon-framework/src/frontController.php';
