5c35c66dc5b5f5ae85253a5a16e7bf0e913d3952
olamedia <olamedia@gmail.com>
2010-04-03 07:48:21 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@160 05b5fde2-2f63-11df-a735-6ff41f51f3aa

---
+++ src/forms/controls/imageFileInput.php
@@ -0,0 +1,47 @@
+<?php
+class imageFileInput extends fileInput{
+	protected $_filesPrefix = 'l_';
+	public function checkIsImage(){
+		if (!$this->isImage($this->getValue())){
+			$this->setValue('');
+		}
+	}
+	protected function isImage($filename){
+		if ($filename == '') return false;
+		if ($info = getimagesize($filename)){
+			if (in_array($info[2], array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG))){
+				return true;
+			}
+		}
+		return false;
+	}
+	public function afterSave(){
+		$files = $this->_files();
+		$name = $this->getPostName();
+		if (!isset($files[$name])) {
+			return;
+		}
+		$key = $this->getPostKey();
+		if ($key === null){
+			$file = $files[$name];
+		}else{
+			if (!isset($files[$name][$key])) return;
+			$file = $files[$name][$key];
+		}
+		if (isset($file['tmp_name'])){
+			if ($this->isImage($file['tmp_name'])){
+				if ($fileName = $this->_saveFile($file['tmp_name'], $file['name'])){
+					$this->setValue($fileName);
+					if ($property = $this->getProperty()){
+						if ($property instanceof imageFilenameProperty){
+							$property->unlinkThumbs();
+						}
+					}
+					if ($item = $this->getItem()){
+						$item->save();
+					}
+				}
+			}
+		}
+	}
+}
