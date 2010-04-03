cdf8a1e23acd5aa16028ab25ea97d80af8cad46b
olamedia <olamedia@gmail.com>
2010-04-03 18:53:59 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@189 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/modelExpression.php
+++ src/mvc-model/modelExpression.php
@@ -11,9 +11,9 @@ class modelExpression{
 		if ($this->_right instanceof modelProperty){
 			$this->_right = $this->_right->getValue();
 		}
-		if (is_array($this->_right)){
+		/*if (is_array($this->_right)){
 			$this->_right = implode(",", $this->_right);
-		}
+		}*/
 	}
 	public function getArguments(){
 		return array($this->_left, $this->_right);
@@ -36,8 +36,10 @@ class modelExpression{
 	public function __toString(){
 		$right = $this->getRight();
 		if (in_array(strtoupper($this->_operator), array('IN','NOT IN'))){
-			if (is_array($right)){
-				$right = implode(",", $right);
+			if (is_array($this->_right)){
+				$right = implode(",", $this->_right);
+			}else{
+				$right = $this->_right;
 			}
 			$right = '('.$right.')';
 		}
