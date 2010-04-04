145ab2029c6632f94b52a450576db489374d50e6
olamedia <olamedia@gmail.com>
2010-04-04 19:32:10 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@202 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/modelResultSet.php
+++ src/mvc-model/modelResultSet.php
@@ -54,7 +54,7 @@ class modelResultSet extends modelQueryBuilder implements IteratorAggregate, Cou
 		);
 	}
 	public function execute(){
-		echo $this->getSql();
+		//echo $this->getSql();
 		if ($this->_result === null){
 			if ($this->_result = $this->getStorage()->query($this->getSql())){
 				return true;
