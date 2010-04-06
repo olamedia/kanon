9f110cd1ede460f71c837203e9ec04207c6f8b1a
olamedia <olamedia@gmail.com>
2010-04-06 12:35:46 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@269 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/modelStorage.php
+++ src/mvc-model/modelStorage.php
@@ -288,7 +288,7 @@ class modelStorage{
 		return $collection->getModelClass();
 	}
 	public static function getIndirectTablesJoins($sourceTable, $targetTable, $joinType, $joinOn = ''){
-		$this->_registerForeignKeys(); 
+		$this->registerForeignKeys(); 
 		$keys = &storageRegistry::getInstance()->foreignKeys;
 		$sourceClass = self::getTableModel($sourceTable);
 		$targetClass = self::getTableModel($targetTable);
