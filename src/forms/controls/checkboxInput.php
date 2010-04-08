9f226a46a7a5788c2ef4ba1b73f7b28c96b32d3c
olamedia <olamedia@gmail.com>
2010-04-08 12:48:54 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@288 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/forms/controls/checkboxInput.php
+++ src/forms/controls/checkboxInput.php
@@ -1,6 +1,9 @@
 <?php
 require_once dirname(__FILE__).'/../control.php';
 class checkboxInput extends control{ // 0/1 values
+	/**
+	 * @var string
+	 */
 	protected $_inputCssClass = 'checkbox';
 	public function getHtml(){
 		return '<div style="margin: 5px 0;"><input type="hidden" name="'.$this->getName().'" value="0" />'.
