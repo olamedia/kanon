f3e7dc1aa5b2ff80e8078b030239184c51cd6633
olamedia <olamedia@gmail.com>
2010-04-10 11:18:21 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@305 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/integerProperty.php
+++ src/mvc-model/properties/integerProperty.php
@@ -4,10 +4,4 @@ class integerProperty extends modelProperty{
 	protected $_dataType = modelProperty::TYPE_INTEGER;
 	protected $_dataSize = 20;
 	protected $_dataUnsigned = false;
-	public function getCreateSql(){
-		$type = 'bigint';
-		if ($this->_size < 11) $type = 'int';
-		if ($this->_size < 4) $type = 'tinyint';
-		return "`".$this->_fieldName."` ".$type."(".$this->_size.")".($this->_unsigned?" unsigned":'').($this->_notNull?" NOT NULL":'');
-	}
 }
--- src/mvc-model/properties/textProperty.php
+++ src/mvc-model/properties/textProperty.php
@@ -2,7 +2,4 @@
 require_once dirname(__FILE__).'/stringProperty.php';
 class textProperty extends stringProperty{
 	protected $_dataType = modelProperty::TYPE_TEXT;
-	public function getCreateSql(){
-		return "`".$this->_fieldName."` longtext".($this->_notNull?" NOT NULL":'');
-	}
 }
