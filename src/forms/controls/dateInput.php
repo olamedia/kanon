68fda0f4628823037ee1fc15387f0caff2ab7fc7
olamedia <olamedia@gmail.com>
2010-04-12 13:46:23 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@347 05b5fde2-2f63-11df-a735-6ff41f51f3aa

---
+++ src/forms/controls/dateInput.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__FILE__).'/textInput.php';
+class dateInput extends textInput{
+	public function setValue($value){
+		if (preg_match("#^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$#ims", $value, $subs)){
+			$m = ltrim($subs[2], '0');
+			$d = ltrim($subs[1], '0');
+			$y = $subs[3];
+			$timestamp = mktime(0,0,0,$m,$d,$y);
+			parent::setValue($timestamp);
+		}
+	}
+	public function getValue(){
+		$value = parent::getValue();
+		return date("d.m.Y", $value); 
+	}
+}
