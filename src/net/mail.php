c1451354721101afd8a3ca1ad435e47bffee71e5
olamedia <olamedia@gmail.com>
2010-05-03 12:45:39 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@382 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/net/mail.php
+++ src/net/mail.php
@@ -1,1 +1,6 @@
 <?php
+// Simple mail function with MIME headers and base64 encoded subject
+function kanon_mail($to, $topic, $message, $headers, $charset = 'UTF-8'){
+	$subject = '=?'.$charset.'?B?'.base64_encode($topic).'?=';
+	return mail($to, $subject, chunk_split(base64_encode($message)), "MIME-Version: 1.0\r\nContent-Transfer-Encoding: BASE64\r\n".$headers);
+}
