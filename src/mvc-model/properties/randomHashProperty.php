2edb19ec97f2255894e666c719a2977f1e355faa
olamedia <olamedia@gmail.com>
2010-05-03 12:56:12 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@385 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/randomHashProperty.php
+++ src/mvc-model/properties/randomHashProperty.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__FILE__).'/stringProperty.php';
 class randomHashProperty extends stringProperty{
 	public function generate(){
 		srand((double) microtime() * 1000000);
