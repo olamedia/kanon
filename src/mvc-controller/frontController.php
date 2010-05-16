2a4544510c10584fec010be31443750d6a7edf8a
olamedia <olamedia@gmail.com>
2010-05-16 13:17:35 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@424 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-controller/frontController.php
+++ src/mvc-controller/frontController.php
@@ -5,8 +5,9 @@
 require_once dirname(__FILE__).'/controller.php';
 class frontController extends controller{
 	public static function startSession($domain, $expire = 360000) {
+		ini_set("session.gc_maxlifetime", $expire);
 		session_set_cookie_params($expire, '/', $domain);
-		@session_start();
+		@session_start(); 
 		// Reset the expiration time upon page load
 		if (isset($_COOKIE[session_name()])){
 			setcookie(session_name(), $_COOKIE[session_name()], time() + $expire, "/", $domain);
--- src/mvc-model/properties/timestampProperty.php
+++ src/mvc-model/properties/timestampProperty.php
@@ -15,4 +15,10 @@ class timestampProperty extends integerProperty{
 		$ma = array(null,'Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек');
 		return $wa[date("w", $ts)].' '.date("d", $ts).' '.$ma[date("n", $ts)].' '.date("Y H:i:s", $ts);
 	}
+	public function html5(){ // html5 <time datetime="">
+		// P - Difference to Greenwich time (GMT) with colon between hours and minutes (added in PHP 5.1.3)  	Example: +02:00
+		// c - ISO 8601 date Example: 2004-02-12T15:19:21+00:00
+		return date("c");
+		return date("Y-m-d").'T'.date("H:i:sP");
+	}
 }
