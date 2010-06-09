401113bbf31e0b343c0fe4f4d659dfbc889523a2
olamedia <olamedia@gmail.com>
2010-06-09 06:44:47 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@461 05b5fde2-2f63-11df-a735-6ff41f51f3aa

---
+++ src/mvc-controller/listController.php
@@ -0,0 +1,36 @@
+<?php
+class listController extends controller{
+	protected $_model = null;
+	protected $_title = 'title';
+	protected $_subController = null;
+	public function _action($action){
+		//$app = application::getInstance();
+		$modelId = intval($action);
+		$class = get_class($this);
+		$cl = strlen($class);
+		$lc = 'ListController';
+		$l = strlen($lc);
+		if ($this->_model === null){
+			if (substr($class,$cl-$l,$l)==$lc){
+				$this->_model = substr($class,0,$cl-$l);
+			}
+			if (($this->_model === null) || (!class_exists($this->_model))){
+				throw new Exception('setup $_model in '.get_class($this));
+			}
+		}
+		$model = $this->_model;
+		if ($subController === null){
+			$this->_subController = $model.'Controller';
+			if (!class_exists($this->_subController)){
+				throw new Exception('setup $_subController in '.get_class($this));
+			}
+		}
+		$subController = $this->_subController;
+		$items = modelCollection::getInstance($model);
+		$item = $items->select()->where("$items->id = '$modelId'")->fetch();
+		if ($item){
+			$this->appendToBreadcrumb(array('<a href="'.$this->rel($modelId).'">'.$item->{$this->_title}->html().'</a>'));
+			$this->runController($subController, array($model => $item));
+		}
+	}
+}
