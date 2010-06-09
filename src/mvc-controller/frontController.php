764560a0ea7ba964078a55251d60b9063e2e9c78
olamedia <olamedia@gmail.com>
2010-06-09 15:48:34 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@465 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-controller/frontController.php
+++ src/mvc-controller/frontController.php
@@ -5,11 +5,12 @@
 require_once dirname(__FILE__).'/controller.php';
 class frontController extends controller{
 	public static function startSession($domain, $expire = 360000) {
+		echo 'Session start';
 		ini_set("session.gc_maxlifetime", $expire);
 		session_set_cookie_params($expire, '/', $domain);
 		if (!session_start()){
 			throw new Exception('session can\'t start');
-		} 
+		}
 		// Reset the expiration time upon page load
 		if (isset($_COOKIE[session_name()])){
 			setcookie(session_name(), $_COOKIE[session_name()], time() + $expire, "/", $domain);
