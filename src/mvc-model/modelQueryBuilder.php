15faf09d822e7a5ce970962a204649a4423d9b4d
olamedia <olamedia@gmail.com>
2010-06-19 11:57:03 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@503 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/modelQueryBuilder.php
+++ src/mvc-model/modelQueryBuilder.php
@@ -363,7 +363,9 @@ class modelQueryBuilder{
 	public function getSqlHtml(){
 		$sql = htmlspecialchars($this->getSql());
 		$m = array("FROM","INNER JOIN","WHERE","AND","OR","GROUP BY");
-		$sql = preg_replace('#('.implode("|", $m).')#ims', '<br /><b>\1</b>', $sql);
+		$pattern = '#('.implode("|", $m).')#imsu';
+		echo $pattern;
+		$sql = preg_replace($pattern, '<br /><b>\1</b>', $sql);
 		return '<div style="padding: 3px;" onClick="$(this).children(\'div\').show();"><b style="color: #24659B">SQL</b><div style="display: none; background: #FFE5BF; padding: 7px;">'.($sql).'</div></div>';
 	}
 	public function &getSql(){
