51699c09d84883afab23b66ce481e44c73593df5
olamedia <olamedia@gmail.com>
2010-06-24 07:23:23 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@539 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/events/event.php
+++ src/events/event.php
@@ -1,11 +1,11 @@
 <?php
-class event{
+class event implements ArrayAccess{
 	protected $_subject = null;
 	protected $_name = '';
 	protected $_parameters = array();
 	protected $_processed = false;
 	protected $_value = null;
-	public function __construct($subject, $name, $parameters = array()){
+	public function __construct($subject, string $name, $parameters = array()){
 		$this->_subject = $subject;
 		$this->_name = $name;
 		$this->_parameters = $parameters;
@@ -16,7 +16,7 @@ class event{
 	public function getSubject(){
 		return $this->_subject;
 	}
-	public function setProcessed($processed){
+	public function setProcessed($processed = true){
 		$this->_processed = (boolean) $processed;
 	}
 	public function isProcessed(){
@@ -27,5 +27,32 @@ class event{
 	}
 	public function getReturnValue(){
 		return $this->_value;
+	}
+	public function hasParameter($name){
+		return ($this->_parameters[$name] !== null);
+	}
+	public function setParameter($name, $value){
+		$this->_parameters[$name] = $value;
+	}
+	public function getParameter($name){
+		if ($this->_parameters[$name] === null){
+			throw new InvalidArgumentException(sprintf('The event "%s" has no "%s" parameter.', $this->_name, $name));
+		}
+		return $this->_parameters[$name];
+	}
+	public function offsetExists(string $name){
+		return ($this->_parameters[$name] !== null); // faster than array_key_exists or isset
+	}
+	public function offsetSet($name, $value){
+		$this->_parameters[$name] = $value;
+	}
+	public function offsetUnset($name){
+		unset($this->_parameters[$name]);
+	}
+	public function offsetGet($name){
+		if ($this->_parameters[$name] === null){
+			throw new InvalidArgumentException(sprintf('The event "%s" has no "%s" parameter.', $this->_name, $name));
+		}
+		return $this->_parameters[$name];
 	}
 }
