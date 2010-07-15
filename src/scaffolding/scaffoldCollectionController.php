6d4d7904ab79bef1fa9879e03e5ed0dbde3a2b47
olamedia <olamedia@gmail.com>
2010-07-15 05:28:45 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@745 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/scaffolding/scaffoldCollectionController.php
+++ src/scaffolding/scaffoldCollectionController.php
@@ -7,13 +7,27 @@ class scaffoldModelCollectionController extends controller{
 	protected $_itemsByPage = 50;
 	public function onConstruct(){
 		$this->_collection = $this->_options['collection'];
+		$this->css('
+		table.scaffold-list{
+			border: solid 1px #666;
+		}
+		.scaffold-list th{
+			background: #eee;
+			border: solid 1px #999;
+			padding: 3px;
+		}
+		.scaffold-list td{
+			border: solid 1px #999;
+			padding: 3px;
+		}
+		');
 	}
 	public function index($page){
 		$this->_collection->setItemsByPage($this->_itemsByPage);
 		$itemsCount = count($this->_collection->select());
 		$pagesCount = ceil($itemsCount/$this->_itemsByPage);
 		$this->viewPages($pagesCount, $page);
-		echo '<table>';
+		echo '<table class="scaffold-list">';
 		$first = true;
 		foreach ($this->_collection->select()->page($page) as $item){
 			$properties = $item->getPropertyNames();
