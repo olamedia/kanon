d0527381996922509a7613595c2968e3ec2ebb3f
olamedia <olamedia@gmail.com>
2010-07-16 04:42:20 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@757 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/common/module.php
+++ src/common/module.php
@@ -1,4 +1,46 @@
 <?php
 class module{
-	
+	protected $_name = '';
+	protected $_isLoaded = false;
+	protected $_autoload = array();
+	protected $_classes = array();
+	public static function getInstance($name){
+		return new self($name);
+	}
+	protected function __construct($name){
+		$this->_name = $name;
+	}
+	public function getName(){
+		return $this->_name;
+	}
+	public function getModels(){
+		$this->_load();
+		return $this->_classes['model'];
+	}
+	protected function _load(){
+		if ($this->_isLoaded) return;
+		static $types = array(
+		'model', 'modelProperty', 
+		'controller', 
+		'controlSet', 'control'
+		);
+		$modulePath = kanon::getBasePath().'/modules/'.$moduleName;
+		if (is_file($modulePath.'/module.php') && is_php($modulePath.'/module.php')){
+			include $modulePath.'/module.php';
+			$this->_autoload = $autoload;
+			foreach ($this->_autoload as $class => $filename){
+				$classFilename = $modulePath.'/'.$filename;
+				if (is_file($classFilename) && is_php($classFilename)){
+					require_once $classFilename;
+					$type = '';
+					$r = new ReflectionClass($class);
+					foreach ($types as $checkType){
+						if ($r->isSubclassOf($checkType)) $type = $checkType;
+					}
+					$this->_classes[$type][$class] = $filename;
+				}
+			}
+		}
+		$this->_isLoaded = true;
+	}
 }
