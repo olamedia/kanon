b48dbbd90535f204a2faccb7a3c958f4831720a5
olamedia <olamedia@gmail.com>
2010-07-19 14:17:58 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@783 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/behaviors/models/commentPrototype.php
+++ src/mvc-model/behaviors/models/commentPrototype.php
@@ -1,4 +1,20 @@
 <?php
 class commentPrototype extends model{
-	
+	protected $_properties = array(
+		'id' => array(
+			'class' => 'idProperty',
+			'field' => 'id',
+			'primaryKey' => true,
+			'autoIncrement' => true,
+		),
+		'threadId' => array(
+			'class' => 'idProperty',
+			'field' => 'thread_id',
+		),
+		'text' => array(
+			'class' => 'textProperty',
+			'field' => 'text',
+		),
+	);
+	protected $_actAs = array('timestampable');
 }
