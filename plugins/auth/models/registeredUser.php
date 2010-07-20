bf99870e3d1eb07005d7bc07dd5672fab89a99f0
olamedia <olamedia@gmail.com>
2010-07-20 20:25:28 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@830 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- plugins/auth/models/registeredUser.php
+++ plugins/auth/models/registeredUser.php
@@ -10,14 +10,10 @@ class registeredUser extends model{
 		'salt' => array(
 			'class' => 'randomHashProperty',
 			'field' => 'salt',
-			'primaryKey' => true,
-			'autoIncrement' => true,
 		),
 		'password' => array(
 			'class' => 'passwordHashProperty',
 			'field' => 'password',
-			'primaryKey' => true,
-			'autoIncrement' => true,
 		),
 	);
 	protected $_actAs = array(
