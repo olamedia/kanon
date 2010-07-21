c1266fcef587bba37cc258610b4dea0769d14fef
olamedia <olamedia@gmail.com>
2010-07-21 21:20:08 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@868 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- plugins/auth/classes/userIdentityPrototype.php
+++ plugins/auth/classes/userIdentityPrototype.php
@@ -1,10 +1,20 @@
 <?php
 /**
- * Authentication: Who is the user?
- * @author olamedia
- *
+ * @desc-ru Личность пользователя, удостоверение.
+ * Основной метод - authenticate(), должен убедиться в подлинности
+ * предъявленного удостоверения (e-mail, url, etc), 
+ * сравнив пароли или используя особые процедуры (openid, oauth etc).
+ * Параметры передаются заранее, в конструкторе.
+ * Специальный метод isRegistered() предоставляет информацию о том, 
+ * был ли зарегистрирован пользователь с таким удостоверением.
+ * Если да, то метод getUserModel() должен вернуть модель с информацией
+ * о пользователе, иначе - новую модель, без данных.
+ * @author		olamedia
+ * @copyright	Copyright © 2010, olamedia
+ * @license		http://www.opensource.org/licenses/mit-license.php MIT
+ * @version		SVN: $Id$
  */
-class userIdentityPrototype{
+abstract class userIdentityPrototype{
 	protected $_isAuthenticated = false;
 	protected $_user = null;
 	protected $_identityModel = null;
