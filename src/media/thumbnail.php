8b5b000cfe52d9a7e413d9d4260fcc8f2f6a72f2
olamedia <olamedia@gmail.com>
2010-07-26 10:35:41 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@910 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/media/thumbnail.php
+++ src/media/thumbnail.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__FILE__).'/image.php';
 class thumbnail extends image{
 	public function __construct($filename, $sourceImage, $rectangle){
 		parent::__construct($filename);
