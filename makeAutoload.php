1946cba9bbc27a7040680748ac86f2af83baa648
olamedia <olamedia@gmail.com>
2010-08-04 15:42:34 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@998 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- makeAutoload.php
+++ makeAutoload.php
@@ -9,26 +9,59 @@ class autoloadGenerator{
 	public function create($filename){
 		$this->_declaredClasses = get_declared_classes();
 		$this->_definedFunction = get_defined_functions();
-		$this->lookup(dirname(__FILE__).'/');
+		$this->lookup(dirname(__FILE__).'/src/');
+		$classes = array();
+		foreach ($this->_classes as $class => $f){
+			$classes[] = "'".$class."'=>'".$f."'";
+		}
+		$classes = implode(",\r\n", $classes);
+		$functions = array();
+		foreach ($this->_functions as $func => $f){
+			$functions[] = "require_once \$dirname.'".$f."';";
+		}
+		$functions = implode("\r\n", $functions);
+		$php = <<<PHPFILE
+<?php
+\$dirname = dirname(__FILE__).'/';
+require_once \$dirname.'src/common/functions/is_php.php';
+\$autoload = array(
+$classes
+);
+kanon::registerAutoload(\$autoload,\$dirname);
+$functions
+PHPFILE;
+		file_put_contents($filename, $php);
+	}
+	public function rel($f){
+		return substr($f, strlen(dirname(__FILE__).'/'));
 	}
 	public function lookFile($f){
-		if (is_php(f)){
+		if (is_php($f)){
+			//echo $f.' ';
 			require_once $f;
 			$declaredClasses = get_declared_classes();
 			$definedFunction = get_defined_functions();
-			$newClasses = array_diff($this->_declaredClasses, $declaredClasses);
-			$newFunctions = array_diff($this->_definedFunction, $definedFunction);
-			var_dump($newClasses);
-			var_dump($newFunctions);
+			$newClasses = array_diff($declaredClasses, $this->_declaredClasses);
+			$newFunctions = array_diff($definedFunction, $this->_definedFunction);
+			foreach ($newClasses as $class){
+				$this->_classes[$class] = $this->rel($f);
+			}
+			foreach ($newFunctions as $func){
+				$this->_functions[$func] = $this->rel($f);
+			}
 		}
 	}
 	public function lookup($dir){
 		foreach (glob($dir.'*') as $f){
+			//echo $f.' ';
 			if (is_dir($f)){
-				$this->lookup($f);
+				$this->lookup($f.'/');
 			}elseif (is_file($f)){
 				$this->lookFile($f);
 			}
 		}
 	}
 }
+
+$generator = new autoloadGenerator();
+$generator->create('kanon-autoload.php');
