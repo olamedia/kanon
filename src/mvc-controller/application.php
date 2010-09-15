8466212f8caf6f8181041c143a6229b9edd3a046
olamedia <olamedia@gmail.com>
2010-09-15 00:31:46 +0000

    refactoring application run
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1122 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-controller/application.php
+++ src/mvc-controller/application.php
@@ -7,11 +7,11 @@ class application{// extends frontController{
     private static $_instance = null;
     public static function getInstance($controllerClassName = null){
         if (self::$_instance === null && $controllerClassName !== null){
+            // autoload all dependencies before session start
+            kanon::loadAllModules();
             self::$_instance = new $controllerClassName();
             response::setStatus(200);
             response::setCharset('utf-8');
-            // autoload all dependencies before session start
-            kanon::loadAllModules();
             @set_magic_quotes_runtime(false);
             frontController::startSession('.'.uri::getDomainName());
             if (get_magic_quotes_gpc ()){
