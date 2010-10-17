8bf924f6289a0e7a581d68913edc823ce63e731f
olamedia <olamedia@gmail.com>
2010-10-17 08:43:26 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1173 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/common/http/request.php
+++ src/common/http/request.php
@@ -6,20 +6,32 @@
  * @author olamedia
  */
 class request{
-    public static function isCli(){
-        return (PHP_SAPI == 'cli');
-    }
-    public static function isAjax(){
-        return 'XMLHttpRequest' == self::getHttpHeader('X-Requested-With');
-    }
-    public static function getMethod(){
-        return $_SERVER['REQUEST_METHOD'];
-    }
-    public static function getHttpHeader($name, $default = null){
-        return self::getServerParameter('HTTP_'.strtoupper(strtr($name, '-', '_')), $default);
-    }
-    public static function getServerParameter($name, $default = null){
-        return isset($_SERVER[$name])?$_SERVER[$name]:$default;
-    }
+	public static function isCli(){
+		return (PHP_SAPI=='cli');
+	}
+	public static function isAjax(){
+		return 'XMLHttpRequest'==self::getHttpHeader('X-Requested-With');
+	}
+	public static function getMethod(){
+		return $_SERVER['REQUEST_METHOD'];
+	}
+	public static function getHttpHeader($name, $default = null){
+		return self::getServerParameter('HTTP_'.strtoupper(strtr($name, '-', '_')), $default);
+	}
+	public static function getServerParameter($name, $default = null){
+		return isset($_SERVER[$name])?$_SERVER[$name]:$default;
+	}
+	/**
+	 * Get current domain name, excluding www. prefix
+	 * @return string Domain name
+	 */
+	public static function getDomainName(){
+		$da = explode(".", self::getServerParameter('SERVER_NAME',''));
+		//reset($da);
+		if (reset($da)=='www'){
+			array_shift($da);
+		}
+		return implode(".", $da);
+	}
 }
 
