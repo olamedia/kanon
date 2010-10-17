56ec2284ee301f4993d103b4ee8deeda0cc8291e
olamedia <olamedia@gmail.com>
2010-10-17 11:24:59 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1178 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-controller/controller.php
+++ src/mvc-controller/controller.php
@@ -106,6 +106,7 @@ class controller extends controllerPrototype{
 		if (count($this->getRegistry()->meta)){
 			foreach ($this->getRegistry()->meta as $name => $meta){
 				if (count($meta) > 1){
+					foreach ($meta as &$x) $x = "$x";
 					$meta = implode(',',$meta);
 				}
 				$h .= '<meta name="'.$name.'" content="'.$meta.'" />';
