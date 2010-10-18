d798f78db22965f39b86e283ff74757b5990e48a
olamedia <olamedia@gmail.com>
2010-10-18 18:35:11 +0000

    magic fix
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1182 05b5fde2-2f63-11df-a735-6ff41f51f3aa

---
+++ src/widgets/widgets.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description of widgets
+ *
+ * @author olamedia
+ */
+class widgets {
+    protected static $_widgets = array();
+    public static function register($id, $widget){
+        self::set($id, $widget);
+    }
+    public static function set($id, $widget){
+        self::$_widgets[$id] = $widget;
+    }
+    public static function get($id, $default = null){
+        return isset(self::$_widgets[$id])?self::$_widgets[$id]:$default;
+    }
+}
+
