40c84ee5cd18ba11eb2ab0adf8143b73aecebcac
olamedia <olamedia@gmail.com>
2010-11-10 14:44:55 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1206 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/integerProperty.php
+++ src/mvc-model/properties/integerProperty.php
@@ -1,11 +1,14 @@
 <?php
 #require_once dirname(__FILE__).'/../modelProperty.php';
+
 class integerProperty extends modelProperty{
 	protected $_dataType = modelProperty::TYPE_INTEGER;
 	protected $_dataSize = 20;
 	protected $_dataUnsigned = false;
 	public function setValue($value){
-		$value = preg_replace('#([^0-9\.\,]+)#ims','',$value);
+		if (is_string($value)){
+			$value = preg_replace('#([^0-9\.\,]+)#ims', '', $value);
+		}
 		$this->_value = $value;
 	}
 }
