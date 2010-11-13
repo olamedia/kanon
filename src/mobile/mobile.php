1af9d6d80a2c479d353b332d4b3b2dff0f8282b5
olamedia <olamedia@gmail.com>
2010-11-13 00:22:05 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1211 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mobile/mobile.php
+++ src/mobile/mobile.php
@@ -13,10 +13,8 @@ class mobile{
 	protected $_ua = '';
 	protected $_nua = '';
 	protected $_profile = '';
-	
 	protected $_browserClass = ''; // opera/webkit/gecko/trident
 	protected $_browserSubclass = ''; // opera mini/firefox/flock/ie7/ie8
-	
 	public static function detect(){
 		$m = new self();
 		$m->setUseragent(request::getUseragent(''));
@@ -29,8 +27,9 @@ class mobile{
 		$ua = str_replace('(', ';', $ua);
 		$ua = str_replace(')', ';', $ua);
 		$ua = explode(';', $ua);
-		foreach ($ua as $k => $v) $ua[$k] = trim($v);
-		$this->_nua = $n = strtolower(';;'.implode(';',$ua).';;');
+		foreach ($ua as $k => $v)
+			$ua[$k] = trim($v);
+		$this->_nua = $n = strtolower(';;'.implode(';', $ua).';;');
 		// Match for Opera Mini
 		// ; Opera Mini/buildnumber;
 		if (strpos(';opera mini/', $n)){
@@ -52,6 +51,14 @@ class mobile{
 		if (strpos(';msie 8.0;', $n)){
 			$this->_browserClass = 'trident';
 			$this->_browserSubclass = 'ie8';
+		}
+		// Match for Webkit browsers
+		// ) AppleWebKit/534.12 (KHTML, like Gecko)
+		if (strpos(';applewebkit/', $n)){
+			$this->_browserClass = 'webkit';
+			if (strpos(';chrome', $n)){
+				$this->_browserSubclass = 'chrome';
+			}
 		}
 	}
 	public function setProfile($rdfLocation){
