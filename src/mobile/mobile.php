b817977c434994ec34dab2520c24cbd6cd21425b
olamedia <olamedia@gmail.com>
2010-11-13 04:56:15 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1213 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mobile/mobile.php
+++ src/mobile/mobile.php
@@ -1,20 +1,30 @@
 <?php
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Nokia:
+ * x-Device-User-Agent
+ * 
+ * 
+ * 
  */
 
 /**
- * Description of mobile
- *
+ * 
+ * @see wurfl
  * @author olamedia
  */
 class mobile{
 	protected $_ua = '';
 	protected $_nua = '';
 	protected $_profile = '';
+	protected $_browserEngine = ''; // opera/webkit/gecko/trident
 	protected $_browserClass = ''; // opera/webkit/gecko/trident
 	protected $_browserSubclass = ''; // opera mini/firefox/flock/ie7/ie8
+	protected $_platform = ''; // win/mac/linux/ios
+	protected $_deviceBrand = '';
+	protected $_isPhone = null; // (device which can call)
+	protected $_j2me = null;
+	protected $_features = array(
+	);
 	public static function detect(){
 		$m = new self();
 		$m->setUseragent(request::getUseragent(''));
@@ -26,24 +36,38 @@ class mobile{
 		// normalize useragent string
 		$ua = str_replace('(', ';', $ua);
 		$ua = str_replace(')', ';', $ua);
+		$ua = str_replace('-', ';', $ua);
+		$ua = str_replace('_', ';', $ua);
+		$ua = str_replace('/', ';', $ua);
 		$ua = explode(';', $ua);
 		foreach ($ua as $k => $v)
 			$ua[$k] = trim($v);
 		$this->_nua = $n = strtolower(';;'.implode(';', $ua).';;');
+		if (strpos($n, ';symbian;')){
+			$this->_platform = 'symbian';
+		}
 		// Match for Opera Mini
 		// ; Opera Mini/buildnumber;
-		if (strpos($n, ';opera mini/')){
+		if (strpos($n, ';opera mini;')){
 			$this->_browserClass = 'opera';
 			$this->_browserSubclass = 'opera mini';
+			$this->_j2me = true;
+			$this->_isPhone = true;
 		}
 		// Match for Opera Mobile
 		// ; Opera Mobi/buildnumber;
-		if (strpos($n, ';opera mobi/')){
+		if (strpos($n, ';opera mobi;')){
 			$this->_browserClass = 'opera';
 			$this->_browserSubclass = 'opera mobile';
+			$this->_isPhone = true;
 		}
 		// Match for MSIE
 		// ; MSIE 8.0;; MSIE 7.0;
+		if (strpos($n, ';msie 6.0;')){
+			$this->_browserEngine = 'trident';
+			$this->_browserClass = 'trident';
+			$this->_browserSubclass = 'ie6';
+		}
 		if (strpos($n, ';msie 7.0;')){
 			$this->_browserClass = 'trident';
 			$this->_browserSubclass = 'ie7';
@@ -52,14 +76,83 @@ class mobile{
 			$this->_browserClass = 'trident';
 			$this->_browserSubclass = 'ie8';
 		}
+		if (strpos($n, ';iemobile')){
+			$this->_browserClass = 'trident';
+			$this->_isPhone = true;
+		}
 		// Match for Webkit browsers
 		// ) AppleWebKit/534.12 (KHTML, like Gecko)
-		if (strpos($n, ';applewebkit/')){
+		if (strpos($n, ';applewebkit;')){
 			$this->_browserClass = 'webkit';
 			if (strpos($n, ';chrome')){
 				$this->_browserSubclass = 'chrome';
 			}
+			if (strpos($n, 'browserng')){
+				$this->_browserSubclass = 'browserng';
+			}
 		}
+		// Mobile browser: UP.Browser
+		if (strpos($n, 'up.browser')){
+			$this->_browserClass = 'up.browser';
+			$this->_browserSubclass = 'up.browser';
+			$this->_isPhone = true;
+		}
+		// Mobile browser: Access Netfront
+		// Browser/NetFront/
+		if (strpos($n, ';netfront;')){
+			$this->_browserClass = 'netfront';
+			$this->_browserSubclass = 'netfront';
+			$this->_isPhone = true;
+		}
+		// Mobile browser: Teleca-Obigo
+		// Browser/Teleca_obigo Obigo/Q05A ObigoInternetBrowser/Q03C Browser/Obigo-Q05A/3.12
+		if (strpos($n, 'teleca')||strpos($n, 'obigo')){
+			$this->_browserClass = 'obigo';
+			$this->_browserSubclass = 'obigo';
+			$this->_isPhone = true;
+		}
+		$this->lookupModel();
+	}
+	protected function lookupModel(){
+		//
+		if (preg_match("#^W3C\-mobile#ims", $this->_ua)){
+			// Brand: W3C XD
+		}
+		if (preg_match("#i(Phone|Pod|Pad)#", $this->_ua)){
+			// Brand: Apple
+			if (preg_match("#\(iPhone;#", $this->_ua)){
+				// iPhone
+			}
+			if (preg_match("#\(iPod;#", $this->_ua)){
+				// iPod
+			}
+			if (preg_match("#\(iPad;#", $this->_ua)){
+				// iPad
+			}
+		}
+		if (preg_match("#^Nokia#ims", $this->_ua)){
+			// Brand: Nokia
+			$this->_deviceBrand = 'nokia';
+		}
+		if (preg_match("#BlackBerry#ims", $this->_ua)){
+			// Brand: BlackBerry
+		}
+		if (preg_match("#^LGE?\-#ims", $this->_ua)){
+			// Brand: LG
+		}
+		if (preg_match("#^MOT\-#ims", $this->_ua)){
+			// Brand: Motorola
+		}
+		if (preg_match("#^SAMSUNG\-#ims", $this->_ua)){
+			// Brand: Samsung
+		}
+		if (preg_match("#^ZTE\-#ims", $this->_ua)){
+			// Brand: ZTE
+		}
+		if (preg_match("#^SonyEricsson#ims", $this->_ua)){
+			// Brand: SonyEricsson
+		}
+		
 	}
 	public function setProfile($rdfLocation){
 		$this->_profile = $rdfLocation;
