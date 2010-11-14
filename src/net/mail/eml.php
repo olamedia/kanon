e56dfd59d9596df31b63439249871d85a34e10cd
olamedia <olamedia@gmail.com>
2010-11-14 12:51:01 +0000

    magic fix
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1239 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/net/mail/eml.php
+++ src/net/mail/eml.php
@@ -1,15 +1,24 @@
 <?php
+ 
 /**
  * Description of eml
  *
  * @author olamedia
  */
-class eml {
-	protected $_attachments = array();
+class eml{
+    protected $_attachments = array();
+    /**
+     * $eml->attach(new emlAttachment(filename($filename)));
+     * @param emlAttachment $attachment
+     * @return eml
+     */
     public function attach($attachment){
-		// do something with attachment
-		$this->_attachments[] = $attachment;
-		return $this;
-	}
+        // do something with attachment
+        if (!($attachment instanceof emlAttachment)){
+            $attachment = new emlAttachment($attachment);
+        }
+        $this->_attachments[] = $attachment;
+        return $this;
+    }
 }
 
