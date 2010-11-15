048fdb46ddd47e7c020e80f51bde9a36d7d9cc42
olamedia <olamedia@gmail.com>
2010-11-15 14:27:36 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1253 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- plugins/sharethis/buttons/twitterShareButton.php
+++ plugins/sharethis/buttons/twitterShareButton.php
@@ -7,7 +7,7 @@
 class twitterShareButton extends shareButton{
 	protected $_domain = 'twitter.com';
     protected $_baseUrl = 'http://twitter.com/home?status=';
-	protected $_tip = 'Опубликовать в Facebook';
+	protected $_tip = 'Опубликовать в Twitter';
 	public function getShareUrl(){
 		return $this->_baseUrl.urlencode($this->getTitle().' - '.$this->getUrl())
 		;
