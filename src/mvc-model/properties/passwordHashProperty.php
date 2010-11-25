7e2622b592f7a1ac330e8513cb3073e49d1ead73
olamedia <olamedia@gmail.com>
2010-11-25 23:11:27 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1296 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/passwordHashProperty.php
+++ src/mvc-model/properties/passwordHashProperty.php
@@ -1,33 +1,38 @@
 <?php
+
 #require_once dirname(__FILE__).'/stringProperty.php';
+
 class passwordHashProperty extends stringProperty{
-	protected $_dataSize = 32;
-	public function getValue(){
-		return '';
-	}
-	public function getSalt(){
-		if (is_object($this->_options['salt'])){
-			$salt = &$this->_options['salt'];
-			if ($salt instanceof randomHashProperty){
-				$salt->generate();
-				$key = $salt->getValue();
-			}else{
-				srand((double) microtime() * 1000000);
-				$key = md5(uniqid(rand()));
-				$this->_options['salt']->setValue($key);
-			}
-			return $key;
-		}else{
-			throw new Exception('salt is not set in passwordHashProperty');
-		}
-	}
-	public function setValue($value){
-		if ($value !== ''){
-			$hash = md5(md5($value).$this->getSalt());
-			$this->_value = $hash;
-		}
-	}
-	public function equals($password){
-		return $this->getInternalValue() == md5(md5($password).$this->getSalt());
-	}
+    protected $_dataSize = 32;
+    public function getValue(){
+        return '';
+    }
+    public function getSalt(){
+        if (is_object($this->_options['salt'])){
+            $salt = &$this->_options['salt'];
+            if ($salt instanceof randomHashProperty){
+                $salt->generate();
+                $key = $salt->getValue();
+            }else{
+                srand((double) microtime() * 1000000);
+                $key = md5(uniqid(rand()));
+                $this->_options['salt']->setValue($key);
+            }
+            return $key;
+        }else{
+            throw new Exception('salt is not set in passwordHashProperty');
+        }
+    }
+    public function setValue($value){
+        if ($value !== ''){
+            $hash = md5(md5($value).$this->getSalt());
+            $this->_value = $hash;
+        }
+    }
+    public function equals($password){
+        return $this->getInternalValue() == $this->getHash($password);
+    }
+    public function getHash($password){
+        return md5(md5($password).$this->getSalt());
+    }
 }
