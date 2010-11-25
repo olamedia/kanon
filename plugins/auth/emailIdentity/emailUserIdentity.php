f1146e60524e06c7ef77801db2cc0fd645cb0ab1
olamedia <olamedia@gmail.com>
2010-11-25 22:18:18 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1290 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- plugins/auth/emailIdentity/emailUserIdentity.php
+++ plugins/auth/emailIdentity/emailUserIdentity.php
@@ -1,53 +1,58 @@
 <?php
 
-class emailUserIdentity extends userIdentityPrototype {
-
-	protected $_email = '';
-	protected $_password = ''; // @todo convert to hash, ex: md5($password)
-
-	public function __construct($email, $password) {
-		$this->_email = $email;
-		$this->_password = $password;
-	}
-
-	/**
-	 * @return boolean whether authentication succeeds
-	 */
-	public function authenticate() {
-		$users = user::getCollection(); //modelCollection::getInstance('registeredUser');
-		$emails = modelCollection::getInstance('userEmail');
-		$result = $users->select($emails, $emails->email->is($this->_email))->fetch();
-		if (!$result) {
-			throw new authException('Email not registered', authException::ERROR_EMAIL_INVALID);
-		}
-		list($user, $userEmail) = $result;
-		$this->_isRegistered = true;
-		if (!$user->password->equals($this->_password)) {
-			throw new authException('Invalid password', authException::ERROR_PASSWORD_INVALID);
-		}
-		$this->_user = $user;
-		$this->_identityModels['email'][$this->_email] = $userEmail;
-	}
-
-	/**
-	 * @return mixed a value that uniquely represents the identity
-	 */
-	public function getId() {
-		return $this->_email;
-	}
-
-	/**
-	 * @return string display name
-	 */
-	public function getName() {
-		return $this->getId();
-	}
-
-	public function register() {
-		$userEmail = new userEmail();
-		$userEmail->userId = $this->_user->id;
-		$userEmail->email = $this->_email;
-		$userEmail->save();
-	}
-
+class emailUserIdentity extends userIdentityPrototype{
+    protected $_email = '';
+    protected $_password = ''; // @todo convert to hash, ex: md5($password)
+    public function __construct($email, $password){
+        $this->_email = $email;
+        $this->_password = $password;
+    }
+    /**
+     * @return boolean whether authentication succeeds
+     */
+    public function authenticate(){
+        $users = user::getCollection(); //modelCollection::getInstance('registeredUser');
+        $emails = modelCollection::getInstance('userEmail');
+        $result = $users->select($emails, $emails->email->is($this->_email))->fetch();
+        if (!$result){
+            throw new authException('Email not registered', authException::ERROR_EMAIL_INVALID);
+        }
+        list($user, $userEmail) = $result;
+        $this->_isRegistered = true;
+        if (!$user->password->equals($this->_password)){
+            throw new authException('Invalid password', authException::ERROR_PASSWORD_INVALID);
+        }
+        $this->_user = $user;
+        $this->_identityModels['email'][$this->_email] = $userEmail;
+    }
+    /**
+     * @return boolean whether authentication succeeds
+     */
+    public function exists(){
+        $users = user::getCollection(); //modelCollection::getInstance('registeredUser');
+        $emails = modelCollection::getInstance('userEmail');
+        $result = $users->select($emails, $emails->email->is($this->_email))->fetch();
+        if (!$result){
+            return false;
+        }
+        return true;
+    }
+    /**
+     * @return mixed a value that uniquely represents the identity
+     */
+    public function getId(){
+        return $this->_email;
+    }
+    /**
+     * @return string display name
+     */
+    public function getName(){
+        return $this->getId();
+    }
+    public function register(){
+        $userEmail = new userEmail();
+        $userEmail->userId = $this->_user->id;
+        $userEmail->email = $this->_email;
+        $userEmail->save();
+    }
 }
