1dc58c9d8410466fee53f442b0ee7be76c358ef6
olamedia <olamedia@gmail.com>
2010-11-26 00:38:57 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1304 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/passwordHashProperty.php
+++ src/mvc-model/properties/passwordHashProperty.php
@@ -7,16 +7,19 @@ class passwordHashProperty extends stringProperty{
     public function getValue(){
         return '';
     }
-    public function getSalt(){
+    public function getSalt($regenerate = false){
         if (is_object($this->_options['salt'])){
             $salt = &$this->_options['salt'];
             if ($salt instanceof randomHashProperty){
-                $salt->generate();
+                if ($regenerate) $salt->generate();
                 $key = $salt->getValue();
             }else{
                 srand((double) microtime() * 1000000);
-                $key = md5(uniqid(rand()));
-                $this->_options['salt']->setValue($key);
+                $key = $this->_options['salt']->getInternalValue();
+                if ($regenerate){
+                    $key = md5(uniqid(rand()));
+                    $this->_options['salt']->setValue($key);
+                }
             }
             return $key;
         }else{
@@ -25,14 +28,14 @@ class passwordHashProperty extends stringProperty{
     }
     public function setValue($value){
         if ($value !== ''){
-            $hash = $this->getHash($value);
+            $hash = $this->getHash($value, true);
             parent::setValue($hash);
         }
     }
     public function equals($password){
         return $this->getInternalValue() == $this->getHash($password);
     }
-    public function getHash($password){
-        return md5(md5($password).$this->getSalt());
+    public function getHash($password,$regenerate = false){
+        return md5(md5($password).$this->getSalt($regenerate));
     }
 }
