d4b151ca1e10f189a06b7b1b9aae552cc12bcc1d
olamedia <olamedia@gmail.com>
2010-12-04 13:33:48 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1321 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/embedProperty.php
+++ src/mvc-model/properties/embedProperty.php
@@ -3,8 +3,8 @@
 class embedProperty extends textProperty{
 	public function html($width = '100%', $height = '400'){
 		$h = $this->getValue();
-		$h = preg_replace("#width=('|\")([0-9%]+)('|\")#ims", 'width="'.$width.'"', $h);
-		$h = preg_replace("#height=('|\")([0-9%]+)('|\")#ims", 'height="'.$height.'"', $h);
-		return $h.'e';
+		$h = preg_replace("#width=('|\")?([0-9%]+)('|\")?#ims", 'width="'.$width.'"', $h);
+		$h = preg_replace("#height=('|\")?([0-9%]+)('|\")?#ims", 'height="'.$height.'"', $h);
+		return $h.'';
 	}
 }
