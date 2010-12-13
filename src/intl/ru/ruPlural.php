0bb301300ed862d6aa08a2c2f8415607e51c590a
olamedia <olamedia@gmail.com>
2010-12-13 12:26:10 +0000

    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1324 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/intl/ru/ruPlural.php
+++ src/intl/ru/ruPlural.php
@@ -6,7 +6,7 @@
  * @author olamedia
  */
 class ruPlural{
-	public function getForm($number, $forms = array(0, 1, 2)){
+	public static function getForm($number, $forms = array(0, 1, 2)){
 		$cases = array(0, 1, 2, 2, 2, 0);
 		return $forms[($number%100>4&&$number%100<20)?0:$cases[min($number%10, 5)]];
 	}
