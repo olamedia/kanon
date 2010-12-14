ead71dd1f37e2261b96c158f5a1d5cd142bfe321
olamedia <olamedia@gmail.com>
2010-12-14 11:51:10 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1325 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-model/properties/stringProperty.php
+++ src/mvc-model/properties/stringProperty.php
@@ -1,5 +1,17 @@
 <?php
-#require_once dirname(__FILE__).'/../modelProperty.php';
 class stringProperty extends modelProperty{
-	protected $_dataType = modelProperty::TYPE_VARCHAR;
+    protected $_dataType = modelProperty::TYPE_VARCHAR;
+    public function ellipsis($maxChars = 100, $ellipsis = '…'){
+        $v = $this->getValue();
+        $l = mb_strlen($v, 'UTF-8');
+        if ($l > $maxChars){
+            $h = floor($maxChars/2);
+            $n = $maxChars-1;
+            if (preg_match("#^(.[$h,$n]\S)\s+#imsu", $v, $subs)){
+                return $subs[1].$ellipsis;
+            }
+            return mb_substr($v, $maxChars).$ellipsis; // cut
+        }
+        return $v;
+    }
 }
