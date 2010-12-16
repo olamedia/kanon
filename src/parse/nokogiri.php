249da31898cb65630597a335a10b2031b2f6cc4d
olamedia <olamedia@gmail.com>
2010-12-16 01:25:42 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1371 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/parse/nokogiri.php
+++ src/parse/nokogiri.php
@@ -135,14 +135,8 @@ class nokogiri implements IteratorAggregate{
             }
         }
         if ($node->hasChildNodes()){
-            if ($node->childNodes->length == 1){
-                $array[$node->firstChild->nodeName] = $this->toArray($node->firstChild);
-            }else{
-                foreach ($node->childNodes as $childNode){
-                    if ($childNode->nodeType != XML_TEXT_NODE){
-                        $array[$childNode->nodeName][] = $this->toArray($childNode);
-                    }
-                }
+            foreach ($node->childNodes as $childNode){
+                $array[$childNode->nodeName][] = $this->toArray($childNode);
             }
         }
         if ($xnode === null){
