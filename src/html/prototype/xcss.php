66f417feb9b6cc676c997a9b5e110a2ccb8ce8ab
olamedia <olamedia@gmail.com>
2011-01-30 04:49:17 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1446 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/html/prototype/xcss.php
+++ src/html/prototype/xcss.php
@@ -3,9 +3,9 @@
 /**
  * Extended css parser (sass inspired http://sass-lang.com/)
  * 
- * $scss = new scss('style.css');
- * echo $scss;
- * $scss->save('style.css');
+ * $xcss = new xcss('style.xcss');
+ * echo $xcss;
+ * $xcss->save('style.css');
  *
  * @author olamedia
  *
@@ -20,6 +20,11 @@
 class xcss{
     protected $_filename = null;
     protected $_source = '';
+    protected $_blocks = array();
+    protected $_vars = array();
+    const
+    XCSS_BLOCK = 1,
+    XCSS_EXPRESSION = 2;
     public function __construct($filename){
         $this->_filename = $filename;
     }
@@ -27,6 +32,7 @@ class xcss{
         if ($this->_source === null){
             $this->_source = file_get_contents($this->_filename);
             $this->_stripComments();
+            $this->_explode();
         }
         return $this->_source;
     }
@@ -36,6 +42,47 @@ class xcss{
                 $this->_source = substr($this->_source, 0, $p).substr($this->_source, $cp);
             }
         }
+    }
+    protected function _explode(){
+        while ($block = $this->_getBlock()){
+            $this->_blocks[] = $block;
+        }
+    }
+    protected function _getBlock(){
+        $type = null;
+        $context = null;
+        $content = null;
+        // 1. looking for { or ;
+        list($c, $p) = $this->_getBlockStart();
+        if ($p === false){
+            // no opening char, return
+            return false;
+        }
+        if ($c == ';'){
+            $type = self::XCSS_EXPRESSION;
+            $content = substr($this->_source, 0, $p);
+        }else{
+            $type = self::XCSS_BLOCK;
+            $context = substr($this->_source, 0, $p);
+            $content = array();
+            $offset = $p;
+            while ($expression = $this->_getBlock()){
+                list($type, $context, $content) = $expression;
+                if ($type != self::XCSS_EXPRESSION){
+                    break;
+                }
+                $content[] = $expression;
+            }
+        }
+        return array($type, $context, $content);
+    }
+    protected function _getBlockStart($offset = 0){
+        // 1. looking for { or ;
+        $bp = strpos($this->_source, '{', $offset);
+        $dp = strpos($this->_source, ';', $offset);
+        if ($bp === false || $dp < $bp)
+            return array(';', $dp);
+        return array('{', $bp);
     }
 }
 
