94632b5a415008e4d74f767a96775d2f92cc55ad
olamedia <olamedia@gmail.com>
2011-01-31 19:22:20 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1532 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/common/http/response/response.php
+++ src/common/http/response/response.php
@@ -138,11 +138,11 @@ class response{
             return;
         }
         if (self::$_lastModified !== null){
-            header('Last-Modified: '.gmdate("D, d M Y H:i:s T", self::$_lastModified).' GMT');
-            header('X-Last-Modified: '.gmdate("D, d M Y H:i:s T", self::$_lastModified).' GMT');
+            header('Last-Modified: '.gmdate("D, d M Y H:i:s T", self::$_lastModified));
+            header('X-Last-Modified: '.gmdate("D, d M Y H:i:s T", self::$_lastModified));
             $ifModifiedSince = request::getHttpHeader('If-Modified-Since', false);
             if ($ifModifiedSince){
-                if ($ifModifiedSince == gmdate('D, d M Y H:i:s T', self::$_lastModified).' GMT'){
+                if ($ifModifiedSince == gmdate('D, d M Y H:i:s T', self::$_lastModified)){
                     self::notModified();
                 }
             }
