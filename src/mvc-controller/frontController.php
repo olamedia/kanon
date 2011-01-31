e37ef28c13e39dbdd739c8103bd9a87da80cca08
olamedia <olamedia@gmail.com>
2011-01-31 18:29:16 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1522 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/mvc-controller/frontController.php
+++ src/mvc-controller/frontController.php
@@ -1,29 +1,31 @@
 <?php
+
 /**
  * $Id$
  */
 #require_once dirname(__FILE__).'/controller.php';
 class frontController extends controller{
-	public static function startSession($domain, $expire = 360000) {
-		ini_set("session.gc_maxlifetime", $expire);
-		session_set_cookie_params($expire, '/', $domain);
-		if (!session_start()){
-			throw new Exception('session can\'t start');
-		}
-		// Reset the expiration time upon page load
-		if (isset($_COOKIE[session_name()])){
-			setcookie(session_name(), $_COOKIE[session_name()], time() + $expire, "/", $domain);
-		}
-		//echo 'Session start';
-	}
-	public static function _stripSlashesDeep(&$value){
-		$value = is_array($value) ?
-		array_map(array(self,'_stripSlashesDeep'), $value) :
-		stripslashes($value);
-		return $value;
-	}
-
+    public static function startSession($domain, $expire = 360000){
+        session_cache_limiter('private_no_expire');
+        ini_set("session.gc_maxlifetime", $expire);
+        session_set_cookie_params($expire, '/', $domain);
+        if (!session_start()){
+            throw new Exception('session can\'t start');
+        }
+        // Reset the expiration time upon page load
+        if (isset($_COOKIE[session_name()])){
+            setcookie(session_name(), $_COOKIE[session_name()], time() + $expire, "/", $domain);
+        }
+        //echo 'Session start';
+    }
+    public static function _stripSlashesDeep(&$value){
+        $value = is_array($value)?
+                array_map(array(self, '_stripSlashesDeep'), $value):
+                stripslashes($value);
+        return $value;
+    }
 }
+
 /*
  * $app = application::getInstance('/');
  * $app->run('/');
