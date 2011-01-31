95ad414c167389ad69eed469a114215022e8a5f0
olamedia <olamedia@gmail.com>
2011-01-31 19:51:36 +0000

    magic rocks
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1537 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/common/http/response/response.php
+++ src/common/http/response/response.php
@@ -179,7 +179,7 @@ class response{
         if ($modify){
             self::modifiedSince(time());
         }else{
-            header('Last-Modified: '.gmdate("D, d M Y H:i:s T", self::$_lastModified));
+            header('Last-Modified: '.gmdate("D, d M Y H:i:s T"));
         }
         header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
         header("Cache-Control: post-check=0, pre-check=0", false);
