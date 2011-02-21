0a4e651568a4e451fa7b0179609aada33ea57a9e
olamedia <olamedia@gmail.com>
2011-02-21 03:21:09 +0000

    importing yProfiler
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1549 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/common/profiler/yProfiler.php
+++ src/common/profiler/yProfiler.php
@@ -108,7 +108,7 @@ class yProfiler{
         return $s;
     }
     public static function html(){
-        sort(self::$_callStatistics);
+        arsort(self::$_callStatistics);
         $stat = array();
         foreach (self::$_callStatistics as $method=>$time){
             $stat[] = $method.' - '.number_format($time, 4, '.', '');
