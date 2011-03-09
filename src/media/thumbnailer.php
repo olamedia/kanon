6ff22b749d6a54b768c8495564798103c449f0f9
olamedia <olamedia@gmail.com>
2011-03-09 08:21:10 +0000

    thumbnailer fix
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1608 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/media/thumbnailer.php
+++ src/media/thumbnailer.php
@@ -134,7 +134,7 @@ class thumbnailer{
         }
     }
     public function readFile($filename){
-        header('X-Served-By: Kanon thumbnailer');
+        header('X-Powered-By: Kanon thumbnailer');
         $mtime = filemtime($filename);
         $gmt = gmdate('r', $timestamp);
         $if = isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])?$_SERVER['HTTP_IF_MODIFIED_SINCE']:'';
