e590364005d297ee03190cef44cbe41176bbd707
olamedia <olamedia@gmail.com>
2011-03-12 08:35:59 +0000

    introducing sitemap
    git-svn-id: http://kanon-framework.googlecode.com/svn/trunk@1649 05b5fde2-2f63-11df-a735-6ff41f51f3aa

--- src/sitemap/ySitemapIndex.php
+++ src/sitemap/ySitemapIndex.php
@@ -40,6 +40,7 @@ class ySitemapIndex{
     public function registerController($name, $controllerName){
         $this->_controllers[$name] = $controllerName;
         $this->add(ySitemap::create('')->setName($name));
+        return $this;
     }
     public function getControllers(){
         return $this->_controllers;
