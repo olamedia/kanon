0b3d81d791bc2655eb50e73608333fa78a0dd697
olamedia <olamedia@gmail.com>
2011-06-06 01:41:26 +0600

    uuid xor

--- incubator/uuid/uuid.php
+++ incubator/uuid/uuid.php
@@ -23,27 +23,41 @@ class uuid{
     public function __construct($uuidString){
         $this->_uuidString = $uuidString;
     }
+    public function _xor($uuid){
+        $result = strval($this->_uuidString);
+        $uuid = strval($uuid);
+        for ($i = 0; $i < 36; $i++){
+            if ($result[$i] !== '-'){
+                $result[$i] = base_convert(
+                        intval(base_convert($result[$i], 16, 10))
+                        ^
+                        intval(base_convert($uuid[$i], 16, 10))
+                        , 10, 16);
+            }
+        }
+        return new uuid($result);
+    }
     /**
      * UUID v4 (random data based)
      * @return string 
      */
     public static function v4(){
         return new uuid(
-                sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                        // 32 bits for "time_low"
-                        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-                        // 16 bits for "time_mid"
-                        mt_rand(0, 0xffff),
-                        // 16 bits for "time_hi_and_version",
-                        // four most significant bits holds version number 4
-                        mt_rand(0, 0x0fff) | 0x4000,
-                        // 16 bits, 8 bits for "clk_seq_hi_res",
-                        // 8 bits for "clk_seq_low",
-                        // two most significant bits holds zero and one for variant DCE1.1
-                        mt_rand(0, 0x3fff) | 0x8000,
-                        // 48 bits for "node"
-                        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-                )
+                        sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                                // 32 bits for "time_low"
+                                mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+                                // 16 bits for "time_mid"
+                                mt_rand(0, 0xffff),
+                                // 16 bits for "time_hi_and_version",
+                                // four most significant bits holds version number 4
+                                mt_rand(0, 0x0fff) | 0x4000,
+                                // 16 bits, 8 bits for "clk_seq_hi_res",
+                                // 8 bits for "clk_seq_low",
+                                // two most significant bits holds zero and one for variant DCE1.1
+                                mt_rand(0, 0x3fff) | 0x8000,
+                                // 48 bits for "node"
+                                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+                        )
         );
     }
     /**
@@ -59,21 +73,21 @@ class uuid{
         }
         $hash = md5($namespace->toBinary().$name);
         return new uuid(
-                sprintf('%08s-%04s-%04x-%04x-%12s',
-                        // 32 bits for "time_low"
-                        substr($hash, 0, 8),
-                        // 16 bits for "time_mid"
-                        substr($hash, 8, 4),
-                        // 16 bits for "time_hi_and_version",
-                        // four most significant bits holds version number 5
-                        (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
-                        // 16 bits, 8 bits for "clk_seq_hi_res",
-                        // 8 bits for "clk_seq_low",
-                        // two most significant bits holds zero and one for variant DCE1.1
-                        (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
-                        // 48 bits for "node"
-                        substr($hash, 20, 12)
-                )
+                        sprintf('%08s-%04s-%04x-%04x-%12s',
+                                // 32 bits for "time_low"
+                                substr($hash, 0, 8),
+                                // 16 bits for "time_mid"
+                                substr($hash, 8, 4),
+                                // 16 bits for "time_hi_and_version",
+                                // four most significant bits holds version number 5
+                                (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+                                // 16 bits, 8 bits for "clk_seq_hi_res",
+                                // 8 bits for "clk_seq_low",
+                                // two most significant bits holds zero and one for variant DCE1.1
+                                (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+                                // 48 bits for "node"
+                                substr($hash, 20, 12)
+                        )
         );
     }
     /**
@@ -89,21 +103,21 @@ class uuid{
         }
         $hash = sha1($namespace->toBinary().$name);
         return new uuid(
-                sprintf('%08s-%04s-%04x-%04x-%12s',
-                        // 32 bits for "time_low"
-                        substr($hash, 0, 8),
-                        // 16 bits for "time_mid"
-                        substr($hash, 8, 4),
-                        // 16 bits for "time_hi_and_version",
-                        // four most significant bits holds version number 5
-                        (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
-                        // 16 bits, 8 bits for "clk_seq_hi_res",
-                        // 8 bits for "clk_seq_low",
-                        // two most significant bits holds zero and one for variant DCE1.1
-                        (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
-                        // 48 bits for "node"
-                        substr($hash, 20, 12)
-                )
+                        sprintf('%08s-%04s-%04x-%04x-%12s',
+                                // 32 bits for "time_low"
+                                substr($hash, 0, 8),
+                                // 16 bits for "time_mid"
+                                substr($hash, 8, 4),
+                                // 16 bits for "time_hi_and_version",
+                                // four most significant bits holds version number 5
+                                (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+                                // 16 bits, 8 bits for "clk_seq_hi_res",
+                                // 8 bits for "clk_seq_low",
+                                // two most significant bits holds zero and one for variant DCE1.1
+                                (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+                                // 48 bits for "node"
+                                substr($hash, 20, 12)
+                        )
         );
     }
     public function toBinary(){
